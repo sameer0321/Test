6e629ebaed7bffd8ba4027d0070cd61642d41a48
sameer0321 <nitinsainsrowa@gmail.com>
2024-03-14 18:00:39 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+let navBar = document.querySelector(".mobileView");
+let menuBtn = document.querySelector(".menu");
+let body = document.querySelector("body");
+let span1 = document.querySelector(".menu-btn-line-1");
+let span2 = document.querySelector(".menu-btn-line-2");
+let navLinks = document.querySelectorAll(".navLinks")
+
+menuBtn.addEventListener("click", function () {
+    navBar.classList.toggle("show");
+    body.classList.toggle("overflow-hidden");
+    span1.classList.toggle("arrow-1");
+    span2.classList.toggle("arrow-2");
+});
+
+navLinks.forEach((links) => {
+    links.addEventListener("click", function () {
+        navBar.classList.remove("show");
+        body.classList.remove("overflow-hidden");
+        span1.classList.remove("arrow1");
+        span2.classList.remove("arrow2");
+    });
+})
+
